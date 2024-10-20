db90d325d5c695ba444c8efb64d3ef33b48a6beb
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-20 21:46:54 +0400

    Create MongoDbApplicationTests.java

---
+++ src/test/java/com/rustam/mongo_db/MongoDbApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rustam.mongo_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
